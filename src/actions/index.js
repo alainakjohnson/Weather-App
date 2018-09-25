bda739e5c4e8542ed1e717fefea06540afee4bb3
alainakjohnson <akjohnson@valdosta.edu>
2018-09-25 17:43:37 +0000

    added weather API key

---
+++ src/actions/index.js
@@ -0,0 +1,1 @@
+const API_KEY = '79ba55c402c38a4b6d534ab0a35e44ac';
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import rootReducer from './reducers';
 import {createStore,applyMiddleware} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import ReduxPromise from 'redux-promise'
+//import {composeWithDevTools} from 'redux-devtools-extension';
 import {Provider} from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-const store = createStore(rootReducer, {}, composeWithDevTools(applyMiddleware()));
+const store = createStore(rootReducer, {}, applyMiddleware(ReduxPromise));
 
 ReactDOM.render(
     <Provider store={store}>
