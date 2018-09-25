0150fad7fee24bf67cabcc7720fb7df48e44c8b8
alainakjohnson <akjohnson@valdosta.edu>
2018-09-25 22:54:33 +0000

    beginning to work on chart portion

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
 class WeatherList extends Component {
     
     renderWeather(cityData){
                     const name = cityData.city.name;
+                    const samples =[10.4,20.2,15.0,30.11,18.45,27.9];
+                    const temp = [];
                     
                     return(
                         <tr key={ name }>
@@ -12,6 +15,29 @@ class WeatherList extends Component {
                             <td>{ cityData.list[0].main.temp }</td>
                             <td>{ cityData.list[0].main.pressure }</td>
                             <td>{ cityData.list[0].main.humidity }</td>
+                            
+                            <td>
+                            <div>
+                            <Sparklines
+                                svgHeight={120}
+                                svgWidth={180}
+                                data={samples}>
+                                <SparklinesLine color="red"/>
+                            </Sparklines>
+                            </div>
+                            </td>
+                            
+                             <td>
+                            <div>
+                            <Sparklines
+                                svgHeight={120}
+                                svgWidth={180}
+                                data={temp}>
+                                <SparklinesLine color="red"/>
+                            </Sparklines>
+                            </div>
+                            </td>
+                            
                         </tr>
                     )
                 }
@@ -19,7 +45,7 @@ class WeatherList extends Component {
     render(){
         
         return(
-            <table className = "table table-hover">
+            <table className="table table-hover">
                 <thead>
                     <tr>    
                         <th>City</th>
