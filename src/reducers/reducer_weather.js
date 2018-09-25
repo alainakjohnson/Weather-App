42d905860c5662cf71cf2709646f75bfd90d417d
alainakjohnson <akjohnson@valdosta.edu>
2018-09-25 21:39:20 +0000

    Weather is displaying properly

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -5,6 +5,6 @@ export default function(state = [], action){
     switch (action.type){
         case FETCH_WEATHER:
             return [ action.payload.data, ...state ];
-    }
+        }
     return state;
 }
