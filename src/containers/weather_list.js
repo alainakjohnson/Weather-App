9e1f84333cc86893c2889e6fa681e6688ce34268
alainakjohnson <akjohnson@valdosta.edu>
2018-09-26 01:59:25 +0000

    charts are working

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,28 +6,19 @@ class WeatherList extends Component {
     
     renderWeather(cityData){
                     const name = cityData.city.name;
-                    const samples =[10.4,20.2,15.0,30.11,18.45,27.9];
-                    const temp = [];
+                    const temp = cityData.list.map(function temp (state) {return state.main.temp});
+                    //console.log(temp)
+                    const pressure = cityData.list.map(function pressure (state) {return state.main.pressure});
+                    //console.log(pressure)
+                    const humidity = cityData.list.map(function humidity (state) {return state.main.humidity});
+                    //console.log(humidity)
+
+                    //temp values for atlanta = 299.22, 297.67, 295.22 etc
                     
                     return(
                         <tr key={ name }>
                             <td>{ name }</td>
-                            <td>{ cityData.list[0].main.temp }</td>
-                            <td>{ cityData.list[0].main.pressure }</td>
-                            <td>{ cityData.list[0].main.humidity }</td>
-                            
                             <td>
-                            <div>
-                            <Sparklines
-                                svgHeight={120}
-                                svgWidth={180}
-                                data={samples}>
-                                <SparklinesLine color="red"/>
-                            </Sparklines>
-                            </div>
-                            </td>
-                            
-                             <td>
                             <div>
                             <Sparklines
                                 svgHeight={120}
@@ -37,6 +28,29 @@ class WeatherList extends Component {
                             </Sparklines>
                             </div>
                             </td>
+                            
+                            <td>
+                            <div>
+                            <Sparklines
+                                svgHeight={120}
+                                svgWidth={180}
+                                data={pressure}>
+                                <SparklinesLine color="blue"/>
+                            </Sparklines>
+                            </div>
+                            </td>
+                            
+                            <td>
+                            <div>
+                            <Sparklines
+                                svgHeight={120}
+                                svgWidth={180}
+                                data={humidity}>
+                                <SparklinesLine color="green"/>
+                            </Sparklines>
+                            </div>
+                            </td>
+
                             
                         </tr>
                     )
@@ -59,9 +73,16 @@ class WeatherList extends Component {
                     {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
+            //this.props.weather.map(this.renderWeather) is an array of items returned by renderWeather
         )
     }
 }
+
+    // ES6 syntax for the following
+    // function mapStateToProps(state){
+    //  return { weather:state.weather };
+    // }
+    // note: this is prob important
 
 function mapStateToProps({ weather }){
     return { weather };
